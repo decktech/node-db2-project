e4c69ad8ee0ac40f781201d29d6f898478e4cf89
Matt Decker <matt@Matts-MBP.lan>
2022-08-16 17:40:16 -0400

    seed created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'supra',
+        mileage: 50000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111123',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 6000,
+        title: 'clean',
+    },
+    {
+        vin: '1111111111234',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 88000,
+        title: 'clean',
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars);
+}

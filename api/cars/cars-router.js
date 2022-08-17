7aa573b1b3c529d1ff0335e1fceee04040c73811
Matt Decker <matt@Matts-MBP.lan>
2022-08-16 22:44:39 -0400

    getAll working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,22 @@
-// DO YOUR MAGIC
+const express = require('express')
+const Car = require('./cars-model')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+
+})
+
+router.post('/', async (req, res, next) => {
+
+})
+
+module.exports = router
